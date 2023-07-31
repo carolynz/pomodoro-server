98ce4d7c7c7c9a76a211c323f401657cf4b18534
Lupe <lupemaydana@gmail.com>
2023-07-31 16:17:24 -0400

    fix: update origin

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "https://basic-chatapp-seven.vercel.app/",
+    origin: "https://basic-chatapp-seven.vercel.app",
     methods: ["GET", "POST"]
   }
 });
