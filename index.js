999ce9c47104d027d974f340d954b95bbc616138
Lupe <lupemaydana@gmail.com>
2023-07-31 15:07:22 -0400

    feat: update creation of server

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
-});
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.send('hola :)');
