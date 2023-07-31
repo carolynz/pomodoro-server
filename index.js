a02053128ce1d13bb94b15a941eae2b509554b34
Lupe <lupemaydana@gmail.com>
2023-07-31 14:29:46 -0400

    feat: add server file

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('hola :)');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  const createdMessage = (msg) => {
+    console.log(msg);
+    socket.broadcast.emit("newIncomingMessage", msg);
+  };
+
+  socket.on("createdMessage", createdMessage);
+
+});
+
+server.listen(4000, () => {
+  console.log('listening on *:4000');
+});
+
