afe7b97131b2cfc8e9e06da81ed937b1e4d64c62
Carolyn Zhang <hi@carolynzhang.com>
2023-08-09 16:07:22 -0400

    server localhost

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "https://basic-chatapp-seven.vercel.app",
-    methods: ["GET", "POST"]
-  }
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-app.get('/', (req, res) => {
-  res.send('hola :)');
+app.get("/", (req, res) => {
+  res.send("hola :)");
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
   const createdMessage = (msg) => {
     console.log(msg);
     socket.broadcast.emit("newIncomingMessage", msg);
   };
 
   socket.on("createdMessage", createdMessage);
-
 });
 
 server.listen(4000, () => {
-  console.log('listening on *:4000');
+  console.log("listening on *:4000");
 });
-
