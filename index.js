29f1b57ae0e281a31183f951d70362e66b835ccc
Carolyn Zhang <hi@carolynzhang.com>
2023-08-15 17:00:07 -0400

    cors array fix

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000, https://pomo.chat, https://www.pomo.chat",
+    origin: [
+      "http://localhost:3000",
+      "https://pomo.chat",
+      "https://www.pomo.chat",
+    ],
     methods: ["GET", "POST"],
   },
 });
